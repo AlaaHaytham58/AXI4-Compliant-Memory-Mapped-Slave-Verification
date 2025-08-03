7a3e5acdab137ca51b82488a2919edb09c1a4a2a
AlaaHaytham58 <alaahaytham910@.com>
2025-08-03 21:56:55 +0300

    Add AXI constraints and enum packages

---
+++ axi_constraints.sv
@@ -0,0 +1,6 @@
+package axi_constraints;
+// Class:AXI constraints read package
+//Class:AXI Constraints write package
+//Class:AXI constraints for handshake signals
+//Class:AXI constraints for memory 
+endpackage
---
+++ axi_enum.sv
@@ -0,0 +1,7 @@
+package axi_enum;
+// might needed enums
+// enum for AXI states
+// AXI states for read and write channels
+//axi handshake states
+
+endpackage 
